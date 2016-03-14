a1bd1a3ed349b33ed89a250e3902e54898bcdd8e
BitiGiti <trnkto@hotmail.com>
2016-03-14 23:23:18 +0100

    metoda ntb

--- page1.php
+++ page1.php
@@ -1,3 +1,23 @@
+        <?php
+            require_once ('book.php');
+            require ('tridy/Db.php');
+            require ('tridy/Dodavatel.php');
+            $connect = new Db("s7000db0105,14330", "PAPR_user", "Dk5.8rQl43-P", "app_licence_test");
+            $connect->conn();
+        $suda = 1;
+
+            $data = mssql_query('SELECT id, CAST(nazev AS TEXT) AS nazev, vek FROM T_test');
+            while($row = mssql_fetch_assoc($data)) 
+            {
+                $nazev = prevod($row['nazev']);
+                $vek = prevod($row['vek']);
+
+                echo ('<tr class="' . ($suda++ % 2 ? "lichy" : "sudy") . '">');
+                echo('<td class="tdo35"><a href="index.php?strana=formdod&dodavatel=1" class="edit">Edit</a></td>');
+                echo('<td class="tdo250">' . $nazev . '</td>');
+                echo('<td class="tdo129">' . $vek . '</td></tr>');
+            }
+        ?>
 <html>
 <head>
 </head>
